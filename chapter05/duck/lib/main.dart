164c4c8eaf5c3743f21976af9ef28035108eb5da
irlirion <irlirion@protonmail.com>
2021-05-23 11:09:02 +0300

    add duck

---
+++ chapter05/duck/lib/main.dart
@@ -0,0 +1,55 @@
+import 'package:flutter/cupertino.dart';
+import 'package:flutter/material.dart';
+
+void main() {
+  runApp(const MyApp());
+}
+
+class MyApp extends StatelessWidget {
+  const MyApp({Key? key}) : super(key: key);
+
+  // This widget is the root of your application.
+  @override
+  Widget build(BuildContext context) {
+    return MaterialApp(
+      title: 'Duck',
+      theme: ThemeData(
+        primarySwatch: Colors.yellow,
+      ),
+      home: const DuckPage(),
+    );
+  }
+}
+
+class DuckPage extends StatefulWidget {
+  const DuckPage({Key? key}) : super(key: key);
+
+  @override
+  State<DuckPage> createState() => _DuckPageState();
+}
+
+class _DuckPageState extends State<DuckPage> {
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+        appBar: AppBar(
+          title: const Text('Duck'),
+        ),
+        body: Stack(
+          alignment: AlignmentDirectional.bottomEnd,
+          children: [
+            const Image(image: AssetImage('assets/images/duck.jpg')),
+            Padding(
+              padding: const EdgeInsets.all(16.0),
+              child: Column(
+                mainAxisSize: MainAxisSize.min,
+                children: const [
+                  Text('It`s a duck!'),
+                  Text('(not a real one)'),
+                ],
+              ),
+            )
+          ],
+        ));
+  }
+}
